65a8a2235b172bf327a871d649de094532af0b29
Shyam Karnani <karnanishyam95@gmail.com>
2025-12-31 19:33:39 +0530

    cleanup: Remove production diagnostic panel

--- App.tsx
+++ App.tsx
@@ -21,7 +21,6 @@ import CartDrawer from './components/CartDrawer';
 import ProductModal from './components/ProductModal';
 import WordPressSyncPanel from './components/WordPressSyncPanel';
 import DebugPanel from './components/DebugPanel';
-import DiagnosticPanel from './components/DiagnosticPanel';
 import { Instagram, Facebook, Twitter, Linkedin, Heart } from 'lucide-react';
 
 const ScrollToTop = () => {
@@ -74,9 +73,6 @@ const AppContent: React.FC = () => {
           </Routes>
         )}
       </main>
-
-      {/* Diagnostic Panel - visible in all modes */}
-      <DiagnosticPanel />
 
       {/* Redesigned Footer: 2-Column Layout */}
       <footer className="bg-[#002366] text-white pt-16 pb-8 px-6">
