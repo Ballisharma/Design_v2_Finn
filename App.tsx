0e4dd162e7c121ba40560063ac21e367d9b273c3
Shyam Karnani <shyamkarnani@Shyams-MacBook-Pro.local>
2026-01-02 22:20:04 +0530

    Update footer background color to #161345

--- App.tsx
+++ App.tsx
@@ -79,7 +79,7 @@ const AppContent: React.FC = () => {
       </main>
 
       {/* Redesigned Footer: 2-Column Layout */}
-      <footer className="bg-[#002366] text-white pt-16 pb-8 px-6">
+      <footer className="bg-funky-dark text-white pt-16 pb-8 px-6">
         <div className="max-w-7xl mx-auto grid grid-cols-1 lg:grid-cols-2 gap-16 lg:gap-32 mb-16">
 
           {/* Column 1: Brand & Socials */}
